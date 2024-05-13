a8ab9416ed3ba64846655e5dbcdd2a1eb15d2cb1
ZairBulos <buloszair@gmail.com>
2024-05-13 17:45:17 -0300

    feat: add initial project

---
+++ src/main/java/com/zair/SpaceXRocketsApiApplication.java
@@ -0,0 +1,13 @@
+package com.zair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceXRocketsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpaceXRocketsApiApplication.class, args);
+	}
+
+}
